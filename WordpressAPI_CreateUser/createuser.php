b39b67d19d6b4efa1833fc61a235d3fedb4d5ded
Adi Sakti <53599610+shaqtee@users.noreply.github.com>
2021-07-01 05:17:54 +0700

    update delete user

--- WordpressAPI_CreateUser/createuser.php
+++ WordpressAPI_CreateUser/createuser.php
@@ -26,3 +26,31 @@
         curl_close($curl);
         echo $response;
         //endNEW USER WORDPRESS
+
+#WP DELETE USER
+$curl = curl_init();
+$idToDelete = '4';
+$administrator = 'abdulbadi';
+$administrapas = '123qweasd';
+$headenc = base64_encode($administrator . ':' . $administrapas);
+//$headauth = base64_decode('YWJkdWxiYWRpOjEyM3F3ZWFzZA==');
+$headauth = 'Authorization: Basic ' . $headenc;
+
+//note reassign=1 adalah admin ID.
+curl_setopt_array($curl, array(
+	CURLOPT_URL => 'http://localhost/sigmago/wp-json/wp/v2/users/' . $idToDelete . '?reassign=1&force=true',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'DELETE',
+	CURLOPT_HTTPHEADER => array($headauth),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+#endDeleteUser
